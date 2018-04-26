16c906dd539c1f327d1984efe2efa3b05227e643
Davide <davidefalcone1@gmail.com>
2018-04-26 19:53:59 +0200

    aggiunte alcune activity e la classe Capo

--- app/src/main/java/com/example/davide/suitup/Login.java
+++ app/src/main/java/com/example/davide/suitup/Login.java
@@ -1,13 +1,33 @@
 package com.example.davide.suitup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private Button vLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        vLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ArmadioActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+
     }
+
+
 }
---
+++ app/src/test/java/DataModel/Capo.java
@@ -0,0 +1,81 @@
+package DataModel;
+
+
+
+public class Capo {
+
+
+    public final int MAGLIA = 0;
+    public final int PANTALONE = 1;
+    public final int SCARPE = 2;
+    public final int ELEGANTE = 0;
+    public final int SPORTIVO = 0;
+    public final int ESTIVO = 0;
+    public final int ROSSO = 0;
+    public final int GIALLO = 1;
+    public final int VERDE = 2;
+    public final int JEANS = 3;
+    public final int BLU = 4;
+    public final int NERO = 5;
+    public final int BEIGE = 6;
+    public final int GRIGIO = 7;
+    public final int BORDEAUX = 8;
+    public final int MARRONE = 9;
+    public final int INVERNALE = 0;
+    public enum Tipo = {MAGLIA, PANTALONE, SCARPE};
+    public enum Stagione = {ESTIVO, INVERNALE};
+    public enum Occasione = {ELEGANTE, SPORTIVO};
+    public enum Colore = {ROSSO, GIALLO, VERDE, JEANS, BLU, NERO ,BEIGE, GRIGIO, BORDEAUX, MARRONE};
+    private String nomeCapo;
+    private Tipo tipo;
+    private Stagione stagione;
+    private Occasione occasione;
+
+    public Capo() {
+    }
+
+    public Capo(String nomeCapo, Tipo tipo, Stagione stagione, Occasione occasione) {
+        this.nomeCapo = nomeCapo;
+        this.tipo = tipo;
+        this.stagione = stagione;
+        this.occasione = occasione;
+    }
+
+    public int getMAGLIA() {
+        return MAGLIA;
+    }
+
+    public String getNomeCapo() {
+        return nomeCapo;
+    }
+
+    public void setNomeCapo(String nomeCapo) {
+        this.nomeCapo = nomeCapo;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public Stagione getStagione() {
+        return stagione;
+    }
+
+    public void setStagione(Stagione stagione) {
+        this.stagione = stagione;
+    }
+
+    public Occasione getOccasione() {
+        return occasione;
+    }
+
+    public void setOccasione(Occasione occasione) {
+        this.occasione = occasione;
+    }
+
+
+}
